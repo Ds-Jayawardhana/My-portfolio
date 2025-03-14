b9edfe8689cb380a594c4cd8523e80ee95e7daea
Disandu Sanhida Jayawardhane <disandu.20230469@iit.ac.lk>
2025-03-14 17:52:42 +0530

    Create all the coponents of the portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a Motivated second-year Computer Science student with expertise in Full Stack Development, UI/UX Design,
+and Machine Learning. Passionate about building scalable applications and optimizing user experiences.
+Actively developing skills through academic projects and freelance work.
+
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
